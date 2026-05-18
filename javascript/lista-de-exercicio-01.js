596128d5098aaeba6c1bf9f1dfe05175c9764b78
Edilton X <ediltonx@edilton.com>
2026-05-18 10:00:26 -0300

    exercicio 1 feito

--- javascript/lista-de-exercicio-01.js
+++ javascript/lista-de-exercicio-01.js
@@ -3,6 +3,13 @@
 utilizando uma estrutura de controle if.
 */
 
+let numero = prompt("Digite um número inteiro: ");
+
+if (numero % 2 === 0) {
+    console.log("O número é par.");
+} else {
+    console.log("O número é ímpar.");
+}
 
 
 
